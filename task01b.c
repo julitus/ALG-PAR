1451f5280ac4e6014dc22ff328e4192d4d813b71
Julio Q <julioqq29@gmail.com>
2017-03-27 08:00:37 -0500

    Task vangrid

--- task01b.c
+++ task01b.c
@@ -50,11 +50,47 @@ struct Matrix three_nested_loop(struct Matrix a, struct Matrix b) {
 	return m;
 }
 
+struct Matrix bloqued_version(struct Matrix a, struct Matrix b, int blocks) {
+	struct Matrix m;
+	m.r = a.r; m.c = b.c;
+	int i, j, k, ii, jj, kk;
+	m.m = malloc(m.r * sizeof(NUM*));
+	/*for(ii = 0; ii < (m.r / blocks); ++ii) {
+		for(jj = 0; jj < (m.c / blocks); ++jj) {
+			for(kk = 0; kk < (a.c / blocks); ++kk) {
+				for(i = ii; i < ii + blocks; ++i) {
+					m.m[i] = malloc(m.c * sizeof(NUM));
+					for(j = jj; j < jj + blocks; ++j) {
+						m.m[i][j] = 0;
+						for(k = kk; k < kk + blocks; ++k)
+							m.m[i][j] = m.m[i][j] + (a.m[i][k] * b.m[k][j]);
+					}
+				}
+			}
+		}
+	}*/
+	for(ii = 0; ii < m.r; ii += blocks) {
+		for(jj = 0; jj < m.c; jj +=  blocks) {
+			for(kk = 0; kk < a.c; kk += blocks) {
+				for(i = ii; i < ii + blocks; ++i) {
+					m.m[i] = malloc(m.c * sizeof(NUM));
+					for(j = jj; j < jj + blocks; ++j) {
+						m.m[i][j] = 0;
+						for(k = kk; k < kk + blocks; ++k)
+							m.m[i][j] = m.m[i][j] + (a.m[i][k] * b.m[k][j]);
+					}
+				}
+			}
+		}
+	}
+	return m;
+}
+
 int main(int argc, char *argv[])
 {
 	srand(time(NULL));
 
-	int n = 1000, blk = 2;
+	int n = 10, blk = 2;
 	struct Matrix m1, m2, p1, p2;
 	m1.r = n; m1.c = n; m2.r = n; m2.c = n;
 
@@ -67,7 +103,11 @@ int main(int argc, char *argv[])
 	printf("\n");*/
 
 	p1 = three_nested_loop(m1, m2);
-	//print(p1);
+	print(p1);
+	printf("\n");
+
+	p2 = bloqued_version(m1, m2, blk);
+	print(p2);
 
 	//valgrind and KCachegrind
 	//sudo apt-get install kubuntu-desktop
